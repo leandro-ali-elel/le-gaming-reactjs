82d128fb90b57c88337e3c2605ad628daf7c6f7a
Ali <ali@cuoma.com>
2022-02-12 10:45:41 -0300

    replaced component with functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HttpClient } from './core/services/HttpClient';
 
-export class App extends React.Component {
-  public games: any[] = [];
-  private httpClient: HttpClient = new HttpClient();
+function App() {
+  const httpClient = new HttpClient();
+  let games: any[] = [];
 
-  public componentDidMount(): void {
-    this.httpClient.get('https://api.rawg.io/api/games?key=80c95efc92c642f1a869a9e0937be512').subscribe(
-      { next: (res: any) => { this.games = res.results; this.forceUpdate() } }
+  useEffect(() => {
+    httpClient.get('https://api.rawg.io/api/games?key=80c95efc92c642f1a869a9e0937be512').subscribe(
+      {
+        next: (res: any) => {
+          console.log(res.results);
+          games.push(res.results);
+        }
+      }
     );
-  }
+  }, [])
 
-  public loopGames(): any {
-    const loop = this.games.map(game => {
+  function loopGames(): any {
+    const loop = games.map(game => {
       const style = {
         backgroundImage: `url(${game.background_image})`
       }
@@ -23,28 +28,25 @@ export class App extends React.Component {
     return <div className='game-container'>{loop}</div>;
   }
 
-  public render() {
-    return (
-      <div className="App">
-        {this.loopGames()}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      {loopGames()}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
